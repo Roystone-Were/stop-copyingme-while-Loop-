797630221d7a3dff027ded3bd548b892e3c05f75
Roystone <80714683+Roystone-Were@users.noreply.github.com>
2025-01-24 02:28:18 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true, // Enable looping for infinite scrolling
+});
